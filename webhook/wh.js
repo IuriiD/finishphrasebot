7836db46f2cf90d409d5321675900fbb28c4579e
Iurii Dziuban <iurii.dziuban@gmail.com>
2018-07-17 13:03:16 +0300

    Almost done

--- webhook/wh.js
+++ webhook/wh.js
@@ -11,6 +11,13 @@ let proverbsIDsSeen = []; // a list of ID's of proverbs that were already shown 
 let anwserVariants = []; // possible answer variants for current question (proverb)
 const proverbsTable = "proverbs"; // name of table with proverb tasks in our DB (finishproverbbot)
 
+let users = {
+    "sessionId": {
+        "proverbsIDsSeen": [],
+        "anwserVariants": []
+    }
+};
+
 
 let app = express();
 app.use(bodyParser.urlencoded({extended: false}));
@@ -29,31 +36,94 @@ app.post("/webhook", async function(req, res) {
     try {
         let payload;
         let ourReq = req.body;
+
         console.log("\nResponse from Dialogflow:\n");
         console.log(JSON.stringify(ourReq, null, 2));
 
         let action = ourReq.result.action;
 
+        let sessionId = ourReq.sessionId;
+
+        // First webhook triggering for a given sessionId
+        if (!(sessionId in users)) {
+            users[sessionId] = { "proverbsIDsSeen": [], "anwserVariants": [] }
+        }
+
+        // In case user's answer is awaited then any his response (including those triggering other intents
+        // from SmallTalk) should be treated as an answer (as in case action="useranswers"); but 2 actions
+        // are exceptions - "restart" and "stop" (there's though a small probability that correct answer should
+        // be a word or phrase from these 2 intents but for now let's leave it as is)
+        let contexts = ourReq.result.contexts;
+        if (contexts.length > 0) {
+            for (let eachContext of contexts) {
+                if (eachContext.name = "useranswers") {
+                    if (action != "restart" && action != "stop") {
+                        action = "useranswers";
+                    }
+                }
+            }
+        }
+
         if (action == "letsplay") {
+            users[sessionId] = { "proverbsIDsSeen": [], "anwserVariants": [] };
             payload = await askProverb("");
         }
 
         else if (action == "useranswers") {
             let usersAnswer = ourReq.result.resolvedQuery;
-            let reactionToPrevAnswer = await checkAnswer(usersAnswer, anwserVariants);
+            let reactionToPrevAnswer = await checkAnswer(usersAnswer, users.sessionId.anwserVariants);
             payload = await askProverb(reactionToPrevAnswer);
         }
 
         else if (action == "restart") {
-            proverbsIDsSeen = [];
+            users[sessionId] = { "proverbsIDsSeen": [], "anwserVariants": [] };
             payload = {
                 "speech": "Ok, let's start from the beginning.. Ready?",
                 "displayText": "Ok, let's start from the beginning.. Ready?",
-                "contextOut": [{
-                    "name": "letsplay",
-                    "parameters": {},
-                    "lifespan": 2
-                }]
+                "contextOut": [
+                    {
+                        "name": "letsplay",
+                        "parameters": {},
+                        "lifespan": 2
+                    },
+                    {
+                        "name": "useranswers",
+                        "parameters": {},
+                        "lifespan": 0
+                    }
+                ]
+            };
+        }
+
+        else if (action == "stop") {
+            users[sessionId] = { "proverbsIDsSeen": [], "anwserVariants": [] };
+            payload = {
+                "speech": "Cancelled! What would you like to do next?",
+                "displayText": "Cancelled! What would you like to do next?",
+                "contextOut": [
+                    {
+                        "name": "letsplay",
+                        "parameters": {},
+                        "lifespan": 0
+                    },
+                    {
+                        "name": "useranswers",
+                        "parameters": {},
+                        "lifespan": 0
+                    }
+                ]
+            };
+        }
+
+        else if (action == "postgresql") {
+            console.log('SQL query');
+            let userInput = ourReq.result.resolvedQuery;
+            let queryFromDF = userInput.split("SQL:")[1];
+            let dbResponse = await getQuery(queryFromDF);
+            payload = {
+                "speech": dbResponse,
+                "displayText": dbResponse,
+                "contextOut": []
             };
         }
 
@@ -61,6 +131,14 @@ app.post("/webhook", async function(req, res) {
 
     } catch (e) {
         console.log(e);
+
+        let payload = {
+            "speech": "Sorry, some error happened. Try again a bit later please..",
+            "displayText": "Sorry, some error happened. Try again a bit later please..",
+            "contextOut": []
+        };
+
+        res.status(200).send(payload);
     }
 });
 
@@ -85,9 +163,9 @@ async function askProverb(reactionToPrevAnswer) {
     let nextProverbID = null;
     for (let i=0; i<proverbsCount; i++) {
         let randN = Math.floor(Math.random() * proverbsCount) + 1;
-        if (!proverbsIDsSeen.includes(randN)) {
+        if (!users.sessionId.proverbsIDsSeen.includes(randN)) {
             nextProverbID = randN;
-            proverbsIDsSeen.push(randN);
+            users.sessionId.proverbsIDsSeen.push(randN);
             break;
         }
     }
@@ -110,31 +188,20 @@ async function askProverb(reactionToPrevAnswer) {
         return payload;
     }
 
-    //
-    /*
-    q = `CREATE TABLE proverbs (id INT, proverbstarts TEXT, proverbends TEXT [])`;
-    dbResponse = await getQuery(q);
-
-        q = `INSERT INTO proverbs VALUES (1, 'Actions speak louder than', '{"word", "words"}'), (2, 'A journey of a thousand miles begins with',  '{"a single step", "single step", "one step", "a step"}'), (3, 'All good things must come to', '{"an end", "end", "finish", "come to end"}'), (4, 'A picture is worth', '{"a thousand words", "thousand words", "thousand of words", "million words", "many words"}'), (5, 'Better late than', '{"never"}')`;
-    dbResponse = await getQuery(q);
-    */
-    //
-
-
     // Request a phrase with chosen ID
     q = `SELECT * FROM ${proverbsTable} where id=${nextProverbID}`;
     dbResponse = await getQuery(q);
     let dbResponseParsed = JSON.parse(dbResponse);
     console.log("\nDB RESPONSE:\n");
-    console.log(dbResponseParsed);
+    //console.log(dbResponseParsed);
 
     let proverbStarts = dbResponseParsed.rows[0].proverbstarts;
-    anwserVariants = dbResponseParsed.rows[0].proverbends;
-    let proverbID = dbResponseParsed.rows[0].id;
+    users.sessionId.anwserVariants = dbResponseParsed.rows[0].proverbends;
+    //let proverbID = dbResponseParsed.rows[0].id;
 
     let speech = reactionToPrevAnswer;
-    console.log("\nproverbsIDsSeen.length: " + proverbsIDsSeen.length);
-    if (proverbsIDsSeen.length == 1) {
+    console.log("\nproverbsIDsSeen.length: " + users.sessionId.proverbsIDsSeen.length);
+    if (users.sessionId.proverbsIDsSeen.length == 1) {
         speech += `Ok. Here's my first question: ${proverbStarts}...`;
     } else {
         speech += ` Next one: ${proverbStarts}...`;
@@ -160,16 +227,24 @@ async function getQuery(q) {
         const { Client } = require('pg');
 
         // Credentials for Heroku
+        /*
         const user = "ydxzwdgaxatmet"; // postgres
         const host = "ec2-54-235-249-33.compute-1.amazonaws.com"; // localhost
         const database = "d6fkk5vc9s6g8p"; // finishproverbbot
+        const dbPort = 5432;
+        */
+
+        // Credentials for local machine
+        const user = "postgres"; // postgres
+        const host = "localhost"; // localhost
+        const database = "finishproverbbot"; // finishproverbbot
         const dbPort = 5432;
 
         const client = new Client({
             user: user,
             host: host,
             database: database,
-            password: process.env.postgreSQLKey,
+            password: process.env.postgreSQLKey || keys.postgreSQLKey,
             port: dbPort,
         });
 
